dbf68b8a792588b623a3fe1212daf8ea9ed03cfc
hillelfaiz <161448769+hillelfaiz@users.noreply.github.com>
2026-07-06 22:36:28 +0700

    Handle OPTIONS in index.php

--- api/index.php
+++ api/index.php
@@ -25,4 +25,14 @@ foreach ($dirs as $dir) {
     }
 }
 
+// Handle CORS for Vercel
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $app->handleRequest(Illuminate\Http\Request::capture());
